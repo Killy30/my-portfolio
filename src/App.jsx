ab145118645222f9a27f2eaca625e81ff5ce4cfc
killy30 <killy17za@gmail.com>
2024-12-21 08:16:15 -0800

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import About from './components/about'
 import Projects from './components/projects'
 import Skills from './components/skills'
 import Contact from './components/contact'
-
+import cors from 'cors'
 import './App.css'
 
 function App() {
@@ -15,16 +15,19 @@ function App() {
     skills:[],
     contacts:{}
   })
+  
+  
+  const URL = 'https://drive.google.com/file/d/17sRo8RqJiqlvyf4OgoYffPoLyOD_ghPj/view?usp=drive_link'
+  const CORS ='https://cors-anywhere.herokuapp.com/'
 
   useEffect(()=>{
-    fetch('../dataPortafolio.json')
+    fetch('../data/dataPortafolio.json')
     .then(res => res.json())
     .then(datas =>{
       setData(datas)
     })
   },[])
   console.log(data);
-  
   
   return (
     <>
--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,13 +1,17 @@
 
-
 function About({about}) {
     
     return(
         <section id="section-2" className="about">
             <div className="borderCard">
-                <div>
-                    <h2>Sobre mi</h2>
-                    <p>{about}</p>
+                <div className="aboutContain">
+                    <div className="cardAboutText">
+                        <h2>Sobre mi</h2>
+                        <p>{about}</p>
+                    </div>
+                    <div className="cardImgSvg">
+                        <img src="/img/undraw__developer.png" alt="" />
+                    </div>
                 </div>
             </div>
         </section>
